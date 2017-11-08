68504da8eed8db4ed3c9375ed9ce9ddd22bdfd80
Quinn O'Neill <quinnoneill@Quinns-MacBook-Pro-2.local>
2017-11-08 11:57:10 -0700

    fills out server.js file

---
+++ back-end/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+ const express = require('express');
+ const app = express();
+ const path = require('path');
+
+ if(!process.env.DYNO) {
+   app.use(function(req, res, next) {
+     res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+     next();
+   });
+ }
+
+ app.use(express.static(__dirname + '/dist'));
+
+ app.get('/*', function(req, res) {
+   res.sendFile(path.join(__dirname + '/dist/index.html'));
+ });
+
+ let port = process.env.PORT || 3000;
+
+ app.listen(port, function() {
+   console.log(`Listening on port ${port}`);
+ });
